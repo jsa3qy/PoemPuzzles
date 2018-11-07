92ca804229e4e4d2593fe537e7cdf23f4a4a1947
jsa3qy <jsa3qy@virginia.edu>
2018-11-07 14:28:48 -0500

    abandoned the parsing for now, now have created list of syllable nodes

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from helpers import *
+from dataCleaningHelpers import *
 from objectDefinitions import *
 
 
 def main():
     myFile = open("poem.txt")
+    otherFile = open("syllablizedPoem.txt")
 
     #No punctuation in words!
     listOfWords = readInRawPoem(myFile)
-    listOfSyls = returnListOfSyllables(listOfWords)
+    listOfSyls = readInSyllablePoem(otherFile)
+    listOfSylNodes = []
+    for numWord, word in enumerate(listOfSyls):
+        for i, syl in enumerate(word):
+            listOfSylNodes.append(sylNode(word, i, str(numWord)+"-"+str(i)))
+    for node in listOfSylNodes:
+        print(node.toString())
 
 
 if __name__ == "__main__":
--- objectDefinitions.py
+++ objectDefinitions.py
@@ -1,17 +1,22 @@
 class sylNode:
     #constructor 1
-    def __init__(self, wordSize, sylRight, sylLeft, tag):
+    def __init__(self, syl, wordSize, sylRight, sylLeft, tag):
         self.wordSize = wordSize
         self.sylRight = sylRight
         self.sylLeft = sylLeft
         self.tag = tag
+        self.syl = syl
 
     #constructor 2
     def __init__(self, listOfSyls, index, tag):
         self.wordSize = len(listOfSyls)
-        self.sylRight = self.wordSize - index + 1
+        self.sylRight = self.wordSize - index - 1
         self.sylLeft = index
         self.tag = tag
+        self.syl = listOfSyls[index]
+
+    def toString(self):
+        print("word size: " + str(self.wordSize) + "\nsylRight: " + str(self.sylRight) + "\nsylLeft: " + str(self.sylLeft) + "\ntag: " + str(self.tag) + "\nsyl: " + str(self.syl))
 
 class ominoe:
     #constructor
