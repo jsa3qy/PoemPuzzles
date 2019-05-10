1a5cf3f4f651b0a1e43c0c55c568f52d85b7a851
jsa3qy <jsa3qy@virginia.edu>
2019-05-10 11:16:35 -0400

    fixed

--- setup_backend.sh
+++ setup_backend.sh
@@ -3,7 +3,7 @@ chmod +x install.sh
 ./install.sh
 cd api
 pip install -r requirements.txt
-chmod +x poem-api.py
+chmod -x poem-api.py
 chmod +x exact_cover_np
 rm -rf exact_cover_np
 git clone https://github.com/moygit/exact_cover_np.git
